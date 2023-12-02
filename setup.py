0d1bde7652e87f3e301286bf19d88b6e530868b5
csaundersshultz <csaundersshultz@gmail.com>
2023-12-02 13:34:07 -0900

    ongoing testing

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup#, find_packages
 
 setup(
     name='hotlink',
     version='0.1',
-    packages=find_packages(),
+    url='https://github.com/csaundersshultz/HotLINK',
+    author='Pablo Saunders-Shultz',
+    author_email='csaundersshultz@gmail.com'
+    license='MIT',
+    description='An example of a python package from pre-existing code'
+    packages=['hotlink', 'hotlink_model', 'example_data'],
     install_requires=[
     	# DEPENDENCIES
     	'numpy',
@@ -12,4 +17,6 @@ setup(
     	'scipy',
     	'scikit-image',
     ],
+    zip_safe=False,
+
 )
