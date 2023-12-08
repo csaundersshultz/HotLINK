8f5cde005464bafd89c964e0cc3e3d11314c7793
csaundersshultz <csaundersshultz@gmail.com>
2023-12-08 11:22:38 -0900

    bugfix

--- hotlink/__init__.py
+++ hotlink/__init__.py
@@ -1,2 +1,2 @@
-from .load_example_data import load_example_data, load_entire_dataset
+from .load_example_data import load_example_data, load_entire_example_dataset
 from .load_hotlink_model import load_hotlink_model
