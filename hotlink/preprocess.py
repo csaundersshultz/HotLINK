3c6c85e48cc975a6fcbadf341a4f0d3264071c6b
Israel Brewster <ijbrewster@alaska.edu>
2025-01-28 08:13:29 -0900

    Cleanup/reafactor preprocess.py

--- hotlink/preprocess.py
+++ hotlink/preprocess.py
@@ -1,15 +1,13 @@
+import math
+import pathlib
+
 import earthaccess
-import glob
-import multiprocessing
 import numpy as np
 import os
 import subprocess
-import time
 import utm
 
-from datetime import datetime, timedelta
-from functools import partial
-from hotlink.support_functions import normalize_MIR, normalize_TIR
+from datetime import datetime
 from pyresample import geometry
 from satpy import Scene
 
@@ -77,101 +75,129 @@ def make_query(dates,bounding_box,sat='modis'):
                 pass
     return results, results1
 
-def download_preprocess(dates,vent,sat='modis',num=5,folder='./data'):
-    lon,lat=vent
+def download_preprocess(dates,vent,sat='modis',batchsize=100,folder='./data'):
+    lat,lon=vent
     bounding_box=(float(lon)-0.05,float(lat)-0.05,float(lon)+0.05,float(lat)+0.05)
-    print(bounding_box)
+
     results,results1=make_query(dates,bounding_box,sat)
+
     if sat in ['viirsj1','viirsj2','viirsn']:
         sat='viirs'
+        
+    area=area_definition('name',vent,sat)
+       
     cwd=os.getcwd()
-    for k in range(int(len(results)/num)):
-        earthaccess.login()
+    
+    num_results = len(results)
+    batches = math.ceil(num_results / batchsize)
+    num = min(num_results + 1, batchsize)
+    
+    dest = pathlib.Path(folder)
+        
+    for k in range(batches):
+        earthaccess.login(persist=True)
         try:
-            print('Descargando2')
-            earthaccess.download(results[num*k:num*k+num], folder, threads=num)
+            print(f'Downloading MIR/Combined files ({k}/{batches})')
+            earthaccess.download(results[num*k:num*k+num], folder, threads=5)
         except Exception as e:
-            print('Fallo descargando2!!!!')
+            print('Downloading Error')
             print(e)
-            print('Intentando nuevamente')
+            print('Trying again with 1 thread')
             earthaccess.download(results[num*k:num*k+num], folder, threads=1)
+            
         if sat=='viirs':
             try:
-                print('Descargando3')
+                print('Downloading VIIRS TIR files ({k}/{batches})')
                 earthaccess.download(results1[num*k:num*k+num], folder, threads=num)
             except Exception as e:
-                print('Fallo descargando3!!!!')
+                print('Download error on VIIRS TIR')
                 print(e)
-                print('Intentando nuevamente')
+                print('Trying again with 1 thread')
                 earthaccess.download(results1[num*k:num*k+num], folder, threads=1)
-        if sat=='modis':
-            ars2=glob.glob(folder+'/MOD0*')
-            ars2+=glob.glob(folder+'/MYD0*')
-        else:
-            ars2=sorted(glob.glob(folder+'/VNP02*'))
-            ars2+=sorted(glob.glob(folder+'/VJ102*'))
-            ars2+=sorted(glob.glob(folder+'/VJ202*'))
-            ars3=sorted(glob.glob(folder+'/VNP03*'))
-            ars3+=sorted(glob.glob(folder+'/VJ103*'))
-            ars3+=sorted(glob.glob(folder+'/VJ203*'))
-        for i in range(len(ars2)):
-            os.chdir(folder)
-            os.chdir(cwd)
-            file=ars2[i]
-            if sat=='viirs':
-                file1=ars3[i]
-                #file1=glob.glob(folder+'/VNP03*'+file.split('.')[1]+'*')
-            fecha=os.path.basename(file).split('.')[1]+'.'+os.path.basename(file).split('.')[2]
-            fecha=datetime.strptime(fecha,'A%Y%j.%H%M')
-            if sat=='viirs':
-                scn=Scene(reader='viirs_l1b',filenames=[folder+'/'+os.path.basename(file),folder+'/'+os.path.basename(file1)])
-            else:
-                scn=Scene(reader='modis_l1b',filenames=[folder+'/'+os.path.basename(file)])
-            if sat=='modis':
-                scn.load(['21','32'],calibration='radiance')
-            else:
-                scn.load(['I04','I05'],calibration='radiance')
-            vent=(lat,lon)
-            area=area_definition('name',vent,sat)
-            if sat=='modis':
-                try:
-                    cropscn = scn.resample(destination=area, datasets=['21','32'])
-                except ValueError:
-                    subprocess.call('rm -rf '+folder+'/'+os.path.basename(file),shell=True)
-                    filename=file.split('/')[-1]
-                    link=results[0].data_links()[0].split(filename.split('.')[0])[0]
-                    link+=filename.split('.')[0]+'/'+filename
-                    subprocess.call('wget -P '+folder+' '+link,shell=True)
-                    scn=Scene(reader='modis_l1b',filenames=[folder+'/'+os.path.basename(file)])
-                    scn.load(['21','32'],calibration='radiance')
-                    cropscn = scn.resample(destination=area, datasets=['21','32'])
-            else:
-                try:
-                    cropscn = scn.resample(destination=area, datasets=['I04','I05'])
-                except ValueError:
-                    print('REINTENTANDO',file,file1)
-                    subprocess.call('rm -rf '+folder+'/'+os.path.basename(file),shell=True)
-                    subprocess.call('rm -rf '+folder+'/'+os.path.basename(file1),shell=True)
-                    filename=file.split('/')[-1]
-                    link=results[0].data_links()[0].split(filename.split('.')[0])[0]
-                    link+=filename.split('.')[0]+'/'+filename
-                    subprocess.call('wget -P '+folder+' '+link,shell=True)
-                    filename1=file1.split('/')[-1]
-                    link1=results1[0].data_links()[0].split(filename1.split('.')[0])[0]
-                    link1+=filename1.split('.')[0]+'/'+filename1
-                    subprocess.call('wget -P '+folder+' '+link1,shell=True)
-                    scn=Scene(reader='viirs_l1b',filenames=[folder+'/'+os.path.basename(file),folder+'/'+os.path.basename(file1)])
-                    scn.load(['I04','I05'],calibration='radiance')
-                    cropscn = scn.resample(destination=area, datasets=['I04','I05'])
-            data=np.ones((70,70,2))*np.nan
-            if sat=='modis':
-                data[:,:,0]=cropscn['21'].values
-                data[:,:,1]=cropscn['32'].values
-            else:
-                data[:,:,0]=cropscn['I04'].values
-                data[:,:,1]=cropscn['I05'].values
-            np.save(folder+'/'+fecha.strftime('%Y%m%d_%H%M.npy'),data)
-            subprocess.call('rm -rf '+folder+'/'+os.path.basename(file),shell=True)
-            if sat=='viirs':
-                subprocess.call('rm -rf '+folder+'/'+os.path.basename(file1),shell=True)
                 
+                
+        if sat=='viirs':
+            ars2=sorted(dest.glob('VNP02*'))
+            ars2+=sorted(dest.glob('VJ102*'))
+            ars2+=sorted(dest.glob('VJ202*'))
+            
+            ars3=sorted(dest.glob('VNP03*'))
+            ars3+=sorted(dest.glob('VJ103*'))
+            ars3+=sorted(dest.glob('VJ203*'))
+        else:
+            ars2=dest.glob('MOD0*')
+            ars2+=dest.glob('MYD0*')           
+            
+            
+        for i in range(len(ars2)):
+            os.chdir(cwd)
+            
+            files=[ars2[i]]
+            reader = 'modis_l1b'
+            if sat=='viirs':
+                reader = 'viirs_l1b'
+                files.append(ars3[i])
+
+            name_parts = files[0].stem.split('.')
+            img_date = datetime.strptime(".".join(name_parts[1:3]), 'A%Y%j.%H%M')
+            try:
+                scn=Scene(reader=reader,filenames=[str(f.absolute()) for f in files])
+                
+                if sat=='viirs':
+                    scn.load(['I04','I05'],calibration='radiance')
+                    try:
+                        cropscn = scn.resample(destination=area, datasets=['I04','I05'])
+                    except ValueError:
+                        file, file1 = files
+                        print('Retrying',str(file),str(file1))
+                        file.unlink()
+                        file1.unlink()                        
+                        filename=file.name
+                        link=results[0].data_links()[0].split(filename.split('.')[0])[0]
+                        link+=filename.split('.')[0]+'/'+filename
+                        subprocess.call('wget -P '+folder+' '+link,shell=True)
+                        filename1=file1.name
+                        link1=results1[0].data_links()[0].split(filename1.split('.')[0])[0]
+                        link1+=filename1.split('.')[0]+'/'+filename1
+                        subprocess.call('wget -P '+folder+' '+link1,shell=True)
+                        
+                        scn=Scene(reader='viirs_l1b',filenames=[str(file.absolute()),str(file1.absolute())])
+                        scn.load(['I04','I05'],calibration='radiance')
+                        cropscn = scn.resample(destination=area, datasets=['I04','I05'])
+                    mir = cropscn['I04'].values
+                    tir = cropscn['I05'].values
+                        
+                else:
+                    scn.load(['21','32'],calibration='radiance')
+                    try:
+                        cropscn = scn.resample(destination=area, datasets=['21','32'])
+                    except ValueError:
+                        file = files[0]
+                        file.unlink()
+                        filename=file.name
+                
+                        link=results[0].data_links()[0].split(filename.split('.')[0])[0]
+                        link+=filename.split('.')[0]+'/'+filename
+                        subprocess.call('wget -P '+folder+' '+link,shell=True)
+                        scn=Scene(reader='modis_l1b',filenames=[str(file.absolute())])
+                        scn.load(['21','32'],calibration='radiance')
+                        cropscn = scn.resample(destination=area, datasets=['21','32'])
+                    mir = cropscn['21'].values
+                    tir = cropscn['32'].values            
+                
+                # Fill missing values
+                min_mir_observered = np.nanmin(mir)                
+                min_tir_observered = np.nanmin(tir)
+                mir[np.isnan(mir)] = min_mir_observered
+                tir[np.isnan(tir)] = min_tir_observered
+                
+                data=np.ones((70,70,2))*np.nan
+                data[:,:,0]=mir
+                data[:,:,1]=tir
+
+                np.save(folder+'/'+img_date.strftime('%Y%m%d_%H%M.npy'),data)
+            except:
+                print(f"Unable to process {file}. Skipping")
+            finally:
+                for file in files:
+                    file.unlink()
