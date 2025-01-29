e9c9445a9d2d9e791903c04c2ea8ce969f742acb
Israel Brewster <ijbrewster@alaska.edu>
2025-01-28 15:04:02 -0900

    Output results as a CSV

--- run_hotlink.py
+++ run_hotlink.py
@@ -1,3 +1,5 @@
+import time
+
 import HotLINK_runner
 
 if __name__ == "__main__":
@@ -10,9 +12,13 @@ if __name__ == "__main__":
     vent = "Shishaldin"
     elevation = 2857
 
-    dates = ("2019-07-15", "2019-07-21") # Year-month, from to.
+    dates = ("2019-07-01", "2019-07-21") # YYYY-MM-DD HH:MM, from to.
+
     # Options: modis,viirs,viirsj2,viirsj1,viirsn
     sensor = 'viirs'
 
+    t1 = time.time()
     results = HotLINK_runner.get_results(vent, elevation, dates, sensor)
     print(results)
+    results.to_csv('HotLINK Results.csv', index = False)
+    print(f"Calculated results in {time.time() - t1}")
